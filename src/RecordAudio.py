3d0bcd4655513e981034c3552735a3ad24e2437e
pouyazhd <p.zahedi@live.com>
2023-02-28 18:10:41 +0330

    Todo list writes

--- src/RecordAudio.py
+++ src/RecordAudio.py
@@ -1,6 +1,11 @@
 import pyaudio
+import numpy as np
 
-class MicRecoder():
-    def __init__(self)->None:
+
+class MicRecoder:
+    def __init__(self) -> None:
+        pyaudio.Stream()
         pass
-    
+
+    def capture(self, time: int = 10) -> np.ndarray:
+        pass
