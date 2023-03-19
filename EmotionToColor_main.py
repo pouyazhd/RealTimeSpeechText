9ba502ffa9122bf8fadb3b6077cefb64c7813431
pouyazhd <p.zahedi@live.com>
2023-03-19 15:48:25 +0330

    Speech recognition fixed with fa-IR and en-us lang. Config file added

--- EmotionToColor_main.py
+++ EmotionToColor_main.py
@@ -1,5 +1,14 @@
 import src.RecordAudio as rec
 import src.SpeechText as sp
+import  yaml
+
+
+def load_configs(config_file:str="SpeachSentimentConfigs.yaml") -> dict:
+    '''
+    load configs
+    :param config_file: input config file address
+    :return: dict of configs
+    '''
 
 def main():
     # generate instances
@@ -8,6 +17,7 @@ def main():
     # recorder.save_audio(audio_frames, "recorded.wav")
     recognizer = sp.SpeechRecognizer()
     recognizer.speech_text()
+
 
 if __name__ == '__main__':
     main()
--- src/SpeechText.py
+++ src/SpeechText.py
@@ -2,30 +2,33 @@ import speech_recognition as sr
 
 
 class SpeechRecognizer:
-    def __int__(self):
-        self.r = sr.Recognizer()
+    def __init__(self) -> None:
+        self.recognizer_object = sr.Recognizer()
 
     def speech_text(self, audio_frames: list = None):
         with sr.Microphone() as source:
-            audio = self.r.listen(source, timeout=10)
-        text = self.r.recognize_google(audio)
-        print(text)
-
+            audio = self.recognizer_object.listen(source,
+                                                  timeout=10,
+                                                  phrase_time_limit=3)
+        text = self.recognizer_object.recognize_google(audio,
+                                                       language="fa-IR",
+                                                       )
+        print(type(text))
 
 # obtain audio from the microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source, timeout=10)
-
-print("audio recorded. Now trying to use Google to recognize it")
-# recognize speech using Google Speech Recognition
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+# r = sr.Recognizer()
+# with sr.Microphone() as source:
+#     print("Say something!")
+#     audio = r.listen(source, timeout=10)
+#
+# print("audio recorded. Now trying to use Google to recognize it")
+# # recognize speech using Google Speech Recognition
+# try:
+#     # for testing purposes, we're just using the default API key
+#     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+#     # instead of `r.recognize_google(audio)`
+#     print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+# except sr.UnknownValueError:
+#     print("Google Speech Recognition could not understand audio")
+# except sr.RequestError as e:
+#     print("Could not request results from Google Speech Recognition service; {0}".format(e))
